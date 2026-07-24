e4a76b0b56e14c0b5917dba8d8902629b57f8d67
clientrail1104 <clientrail@gmail.com>
2026-07-24 16:27:55 +0800

    script-r1.js

--- script.js
+++ script.js
@@ -1,12 +1,53 @@
-window.addEventListener(
-"scroll",
-()=>{
+<script>
 
-let device =
-document.querySelector(".device");
+const counters=document.querySelectorAll(".counter");
 
+counters.forEach(counter=>{
 
-device.style.transform =
-`rotateY(${window.scrollY/20}deg)`;
+const update=()=>{
+
+const target=+counter.dataset.target;
+
+const count=+counter.innerText;
+
+const speed=40;
+
+const increment=target/speed;
+
+if(count<target){
+
+counter.innerText=Math.ceil(count+increment);
+
+requestAnimationFrame(update);
+
+}else{
+
+counter.innerText=target.toLocaleString();
+
+}
+
+}
+
+update();
 
 });
+
+
+const cards=document.querySelectorAll(".card,.metric");
+
+window.addEventListener("mousemove",(e)=>{
+
+const x=e.clientX/window.innerWidth;
+const y=e.clientY/window.innerHeight;
+
+cards.forEach(card=>{
+
+card.style.transform=
+`rotateY(${(x-.5)*8}deg)
+ rotateX(${(0.5-y)*8}deg)`;
+
+});
+
+});
+
+</script>
